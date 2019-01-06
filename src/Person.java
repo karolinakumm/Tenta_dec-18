9eda767aeae68f0637950da5ac19c9f471d2927c
karolinakumm <44191654+karolinakumm@users.noreply.github.com>
2019-01-06 17:27:50 +0100

    ändrat lite

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,5 @@
 
-public class U3_Person {
+public class Person {
 	private String fnamn;
 	private String enamn;
 
@@ -21,22 +21,22 @@ public class U3_Person {
 
 	// Daniels metod, Objektet som matas in här måste dock vara en instans av klassen Person, är Objektet av en annan typ kommer programmet att krascha
 	public boolean equals(Object t) { //åtkomstmetod: public, returtyp: boolean(true/false), metodnamn: equals, variabeltyp: Object, variabelnamn: t.
-			U3_Person p = (U3_Person) t; //Här castar vi det inmatade Objektet till en Person
-			return fnamn.equals(p.fnamn) && enamn.equals(p.enamn);
+			Person p = (Person) t; //Här castar vi det inmatade Objektet till en Person
+			return this.fnamn.equals(p.fnamn) && this.enamn.equals(p.enamn);
 		}
 
 	// metodalternativ 2, I den här metoden använder en if-sats som först kollar att Objektet är en instans av klassen Person innan den går vidare och kör koden.
 	public boolean equals1(Object t) {
-		if (t instanceof U3_Person) { // instanceof är en inbyggd metod som ser ifall Object är en instans av den specificerade typen, i detta fall klassen Person
-			U3_Person p = (U3_Person) t;
+		if (t instanceof Person) { // instanceof är en inbyggd metod som ser ifall Object är en instans av den specificerade typen, i detta fall klassen Person
+			Person p = (Person) t;
 			return p.getFnamn().equals(this.fnamn) && p.getEnamn().equals(this.enamn);
 		}
 		return false;
 	}
 	//metodalternativ 3, här har vi gjort som i ovan bara att vi har ersatt if-satsen med &&. I detta fallet ger de samma resultat men mindre att skriva. Personligen anser jag dock att if-satsen gör det lättare att läsa.
 	public boolean equals2(Object t) {
-		return t instanceof U3_Person && ((U3_Person) t).getFnamn().equals(this.fnamn)
-				&& ((U3_Person) t).getEnamn().equals(this.enamn);
+		return t instanceof Person && ((Person) t).getFnamn().equals(this.fnamn)
+				&& ((Person) t).getEnamn().equals(this.enamn);
 	}
 
 }
